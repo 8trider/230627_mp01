bb6274b96fb3fd61c28275c0130d9945e62c2cb1
8trider <8trider@naver.com>
2023-06-28 09:35:23 +0900

    01

--- pages/장르.py
+++ pages/장르.py
@@ -2,7 +2,42 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
 import streamlit as st
+from pywaffle import Waffle
 import plotly.graph_objects as go
 import common
 import pandas as pd
 
+common.page_config()
+
+st.title("장르 현활")
+
+data_2021 = common.get_2021()
+data_2022 = common.get_2022()
+common.get_preprocessing(data_2021, data_2022)
+
+df_2021 = data_2021[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
+df_2022 = data_2022[['영화명', '개봉일', '매출액', '매출액점유율', '누적매출액', '관객수', '누적관객수', '스크린수', '대표국적', '등급', '대표장르']]
+
+df_2021.loc[:,'대표장르']
+gc_2021 = df_2021.loc[:,'대표장르'].value_counts()
+
+gc_2021_modified = {}
+for key, value in gc_2021.items():
+    gc_2021_modified[key] = value // 20
+fig = plt.figure(
+    FigureClass=Waffle,
+    plots={
+        111: {
+            'values': gc_2021_modified,
+            'labels': ["{0} ({1})".format(n, v) for n, v in gc_2021.items()],
+            'legend': {'loc': 'upper left', 'bbox_to_anchor': (1, 1), 'fontsize': 8},
+            'title': {'label': '2021 장르 (20개당 별 하나)', 'loc': 'left'}
+        }
+    },
+    rows=15,
+    figsize=(15, 5),
+    font_size=15,
+    icons ='star'
+)
+plt.show()
+st.pyplot(fig)
